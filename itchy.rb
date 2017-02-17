278b7ef8111cfebed92631b1955127c4f0eba061
Matt J. Wiater <mwiater@users.noreply.github.com>
2017-02-17 14:57:59 -0800

    Update itchy.rb

--- itchy.rb
+++ itchy.rb
@@ -1,6 +1,6 @@
 require 'dcell'
 
-DCell.start :id => "itchy", :addr => "tcp://0.0.0.0:9001", :registry => { :adapter => 'redis', :host => "192.168.99.104" }
+DCell.start :id => "itchy", :addr => "tcp://0.0.0.0:9001", :registry => { :adapter => 'redis', :host => ENV['REDIS_HOST] }
 
 class Itchy
   include Celluloid
